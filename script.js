7b106e5a4d012e5b740c26fecc4ff2695239a1ae
Ryan Schutte <rmschutte09@gmail.com>
2026-03-10 14:01:38 -0400

    we getting there, turn function made and most things working on the base. Need sprites, fighting logic, then game progression

--- script.js
+++ script.js
@@ -7,47 +7,66 @@ let p2 = document.getElementById("p2")
 let p3 = document.getElementById("p3")
 let pSprite = document.getElementById("playerSprite")
 let oSprite = document.getElementById("opponentSprite")
-let actions = ["guard", "guard break", "thrust", "counter"]
+let actions = ["guard", "guardBreak", "thrust", "counter"]
+let images = ["guard.png", "guardBreak.png", "thrust.png", "counter.png"]
 let battles = [1, 2, 3, 4]
 let playerLife = 1
+let counterCounter = 0;
 
-guard.addEventListener("click", function(){
-    chooseAction("guard");
-    eAct();
-})
-guardBreak.addEventListener("click", function(){
-    chooseAction("guardBreak");
-    eAct();
-})
-thrust.addEventListener("click", function(){
-    chooseAction("thrust");
-    eAct();
-})
-counter.addEventListener("click", function(){
-    chooseAction("counter");
-    eAct();
-})
+window.addEventListener("load", battle)
+
+
+function battle(){
+    let counterCounter = 0
+    guard.addEventListener("click", function(){
+        chooseAction("guard");
+    })
+    guardBreak.addEventListener("click", function(){
+        chooseAction("guardBreak");
+    })
+    thrust.addEventListener("click", function(){
+        chooseAction("thrust");
+    })
+    counter.addEventListener("click", function(){
+        chooseAction("counter");
+    })
+}
 
 function eAct(){
-    let oAct = actions[Math.floor(Math.random()*4)];
-    console.log(oAct);
+    let choice = Math.floor(Math.random()*4)
+    let oAct = actions[choice];
+    oSprite.src = images[choice]
+    return oAct
 }
 
 
 function chooseAction(act){
+    let oAct = eAct()
     if (act == "guard") {
-        console.log(act);
         pSprite.src = "";
     } else if (act == "guardBreak") {
-        console.log(act);
-        pSprite.src = ""
+        pSprite.src = "";
     } else if (act == "counter") {
-        console.log(act);
-        pSprite.src = ""
+        pSprite.src = "";
     } else if (act == "thrust") {
-        console.log(act);
-        pSprite.src = ""
+        pSprite.src = "";
     } else {
 
+    }
+    turn(act, oAct)
+}
+
+function turn(act, oAct){
+    if (counterCounter > 0 && act == "counter"){
+        console.log("You lose");
+        console.log(counterCounter);
+}
+    else if (act == "counter"){
+        console.log(act);
+        console.log(oAct);
+        counterCounter++;
+    } else {
+        console.log(act);
+        console.log(oAct);
     }
 }
